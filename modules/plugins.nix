6adf15d12c660c60b702f003ab6631df5d31ad00
johann <halbachj@tcd.ie>
2026-01-20 22:55:36 +0000

    Reenabled half the plugins. Binray searching the problem

--- modules/plugins.nix
+++ modules/plugins.nix
@@ -16,7 +16,7 @@
     #comments.comment-nvim.enable = true;
     #formatter.conform-nvim.enable = true;
     #diagnostics.nvim-lint.enable = true;
-    #utility.surround.enable = true;
+    utility.surround.enable = true;
     #notes.obsidian = {
     #  enable = true;
     #  setupOpts.workspaces = [
@@ -26,10 +26,10 @@
     #    }
     #  ];
     #};
-    #notes.todo-comments.enable = true;
-    #utility.preview.glow.enable = true;
-    #snippets.luasnip.enable = true;
-    #autopairs.nvim-autopairs.enable = true;
-    #tinymist.enable = true;
+    notes.todo-comments.enable = true;
+    utility.preview.glow.enable = true;
+    snippets.luasnip.enable = true;
+    autopairs.nvim-autopairs.enable = true;
+    tinymist.enable = true;
   };
 }
