786e5100faca434e260d189dd58b04a9689c364f
johann <halbachj@tcd.ie>
2026-01-20 14:28:07 +0000

    Enable treesitter plugin for languages

--- modules/languages.nix
+++ modules/languages.nix
@@ -1,18 +1,19 @@
 { config, lib, pkgs, ... }:
 
 {
-  config.vim.languages = {
-    enableLSP = true;
-    enableTreesitter = true;
+  config.vim = {
+    treesitter.enable = true;
+    languages = {
+      enableLSP = true;
+      enableTreesitter = true;
 
-    nix.enable = true;
-    clang.enable = true;
-    #latex.enable = true;
-    #arduino.enable = true;
-    java.enable = true;
-
+      nix.enable = true;
+      clang.enable = true;
+      #latex.enable = true;
+      #arduino.enable = true;
+      java.enable = true;
+    };
   };
 
 }
-
 
