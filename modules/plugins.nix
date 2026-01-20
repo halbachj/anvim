f3a146351a8118893f53ceab9e89c958747eb5cf
johann <halbachj@tcd.ie>
2026-01-20 22:53:58 +0000

    Removed more plugins

--- modules/plugins.nix
+++ modules/plugins.nix
@@ -5,27 +5,27 @@
                 #./tinymist.nix
   ];
   config.vim = {
-    statusline.lualine.enable = true;
+    #statusline.lualine.enable = true;
     #telescope.enable = true;
     #autocomplete.nvim-cmp.enable = true;
     #binds.whichKey.enable = true;
-    git = {
-    	enable = true;
-    	gitsigns.enable = true;
-    };
+    #git = {
+    #    enable = true;
+    #    gitsigns.enable = true;
+    #};
     #comments.comment-nvim.enable = true;
     #formatter.conform-nvim.enable = true;
     #diagnostics.nvim-lint.enable = true;
     #utility.surround.enable = true;
-    notes.obsidian = {
-      enable = true;
-      setupOpts.workspaces = [
-        {
-          name = "notes";
-          path = "/home/johann/Projects/anvim";
-        }
-      ];
-    };
+    #notes.obsidian = {
+    #  enable = true;
+    #  setupOpts.workspaces = [
+    #    {
+    #      name = "notes";
+    #      path = "/home/johann/Projects/anvim";
+    #    }
+    #  ];
+    #};
     #notes.todo-comments.enable = true;
     #utility.preview.glow.enable = true;
     #snippets.luasnip.enable = true;
