48712bd4dcd0de45ff9642be7736db8caa509474
johann <halbachj@tcd.ie>
2026-01-20 23:00:45 +0000

    Reenabled half the plugins. Binray searching the problem

--- modules/plugins.nix
+++ modules/plugins.nix
@@ -9,10 +9,10 @@
     #telescope.enable = true;
     #autocomplete.nvim-cmp.enable = true;
     #binds.whichKey.enable = true;
-    #git = {
-    #    enable = true;
-    #    gitsigns.enable = true;
-    #};
+    git = {
+        enable = true;
+        gitsigns.enable = true;
+    };
     comments.comment-nvim.enable = true;
     formatter.conform-nvim.enable = true;
     diagnostics.nvim-lint.enable = true;
