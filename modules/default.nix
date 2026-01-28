719480aef6a439d24f0e8b954e2d46b7e1f8e7ed
johann <halbachj@tcd.ie>
2026-01-28 12:52:48 +0000

    Make obsidian workspace per-machine

--- modules/default.nix
+++ modules/default.nix
@@ -1,9 +1,10 @@
 { config, lib, pkgs, ... } : {
-
-  imports = [
-    ./core.nix
-    ./plugins.nix
-    ./languages.nix
-    ./academic.nix
-  ];
+  imports =
+    [
+      ./core.nix
+      ./plugins.nix
+      ./languages.nix
+      ./academic.nix
+    ]
+    ++ lib.optional (builtins.pathExists ./local.nix) ./local.nix;
 }
--- modules/plugins.nix
+++ modules/plugins.nix
@@ -1,35 +1,52 @@
 { config, lib, pkgs, ... }:
 
+let
+  cfg = config.anvim.obsidian;
+in
 {
   imports = [
                 #./tinymist.nix
   ];
-  config.vim = {
-    statusline.lualine.enable = true;
-    telescope.enable = true;
-    autocomplete.nvim-cmp.enable = true;
-    binds.whichKey.enable = true;
-    #git = {
-    #    enable = true;
-    #    gitsigns.enable = true;
-    #};
-    comments.comment-nvim.enable = true;
-    formatter.conform-nvim.enable = true;
-    diagnostics.nvim-lint.enable = true;
-    utility.surround.enable = true;
-    notes.obsidian = {
-      enable = true;
-      setupOpts.workspaces = [
-        {
-          name = "notes";
-          path = "/home/johann/Projects/anvim";
-        }
-      ];
-    };
-    notes.todo-comments.enable = true;
-    utility.preview.glow.enable = true;
-    snippets.luasnip.enable = true;
-    autopairs.nvim-autopairs.enable = true;
-    #tinymist.enable = true;
+
+  options.anvim.obsidian.workspacePath = lib.mkOption {
+    type = lib.types.nullOr lib.types.str;
+    default = null;
+    example = "/home/alice/Notes";
+    description = "Absolute path to the Obsidian workspace; when null, Obsidian is disabled.";
   };
+
+  config = lib.mkMerge [
+    {
+      vim = {
+        statusline.lualine.enable = true;
+        telescope.enable = true;
+        autocomplete.nvim-cmp.enable = true;
+        binds.whichKey.enable = true;
+        #git = {
+        #    enable = true;
+        #    gitsigns.enable = true;
+        #};
+        comments.comment-nvim.enable = true;
+        formatter.conform-nvim.enable = true;
+        diagnostics.nvim-lint.enable = true;
+        utility.surround.enable = true;
+        notes.todo-comments.enable = true;
+        utility.preview.glow.enable = true;
+        snippets.luasnip.enable = true;
+        autopairs.nvim-autopairs.enable = true;
+        #tinymist.enable = true;
+      };
+    }
+    (lib.mkIf (cfg.workspacePath != null) {
+      vim.notes.obsidian = {
+        enable = true;
+        setupOpts.workspaces = [
+          {
+            name = "notes";
+            path = cfg.workspacePath;
+          }
+        ];
+      };
+    })
+  ];
 }
